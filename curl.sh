e6d8fdf53542942f12b415fed4d57e322f7aac1a
Ben Hall <ben@benhall.me.uk>
2016-04-18 21:15:11 +0100

    test scripts

---
+++ curl.sh
@@ -0,0 +1,1 @@
+docker run --dns=192.168.99.100 --dns-search="service.consul" -it benhall/curl curl mycontainercom.service.consul
---
+++ test.sh
@@ -0,0 +1,1 @@
+docker run --dns=192.168.99.100 --dns-search="service.consul" -it ubuntu bash
